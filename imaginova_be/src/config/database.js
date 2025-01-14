16f44ae955802c5b9f3f3da8adabeccafe36e4c6
Cahuaee01 <emilia.napolano@outlook.it>
2025-01-14 15:52:42 +0100

    adjusted db configuration for docker usage

--- imaginova_be/src/config/database.js
+++ imaginova_be/src/config/database.js
@@ -16,6 +16,13 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false, // Usa con attenzione
+    },
+  },
   pool: {
     max: 5,
     min: 0,
